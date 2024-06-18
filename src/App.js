dfddb2761ebd6c1e71f3b6ff5a0414fa29100cb4
Aymane Bokhamy <aymanebokhamy2002@gmail.com>
2024-06-18 16:56:39 -0400

    Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Login />
     </div>
   );
 }
